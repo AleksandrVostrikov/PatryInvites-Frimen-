6db2c028b8bcdcfa1de774b51a4decd985b4ec10
AleksandrVostrikov <vostrik-aleksandr@yandex.ru>
2023-03-21 23:33:44 +0300

    21.03

---
+++ Models/Repository.cs
@@ -0,0 +1,12 @@
+﻿namespace PatryInvites_Frimen_.Models
+{
+    public static class Repository
+    {
+        private static List<GuestResponse> responses = new();
+        public static IEnumerable<GuestResponse> Responses => responses;
+        public static void AddResponse(GuestResponse response)
+        {
+            responses.Add(response);
+        }
+    }
+}
