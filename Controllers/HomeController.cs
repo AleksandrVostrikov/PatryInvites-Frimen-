f4b3fc465cf557dd06c8ca7aea9744fa3b9e599e
AleksandrVostrikov <vostrik-aleksandr@yandex.ru>
2023-03-22 22:35:05 +0300

    22.03

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,8 +18,16 @@ namespace PatryInvites_Frimen_.Controllers
         [HttpPost]
         public ViewResult RsvpForm(GuestResponse guestResponse)
         {
-            Repository.AddResponse(guestResponse);
-            return View("Thanks", guestResponse);
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+            
         }
 
         public ViewResult ListResponses()
--- Models/GuestResponse.cs
+++ Models/GuestResponse.cs
@@ -1,10 +1,16 @@
-﻿namespace PatryInvites_Frimen_.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PatryInvites_Frimen_.Models
 {
     public class GuestResponse
     {
+        [Required(ErrorMessage ="Введите имя")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Введите Email")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Введите телефон")]
         public string Phone { get; set; }
+        [Required(ErrorMessage = "Выберите один из вариантов")]
         public bool? WillAttend { get; set; }
     }
 }
